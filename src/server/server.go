7705f5413d3bd99b894e1eece0ef1a989460876b
llihcchill <125551072+llihcchill@users.noreply.github.com>
2024-09-09 17:56:10 +1000

    Add files via upload

---
+++ src/server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../html/main.html"))
+
+	tmpl.Execute(w, nil)
+}
+
+func serverSelect(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../html/server-select.html"))
+
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/server-select", serverSelect)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
