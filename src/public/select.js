011026661277b3c201be29a1e09e526c7ad0066d
llihcchill <125551072+llihcchill@users.noreply.github.com>
2024-09-24 20:57:44 +1000

    Update select.js
     - Able to render all servers created on /create page
     - Runs socket.emit() only on full page load via htmx.on("htmx:afterSettle")

--- src/public/select.js
+++ src/public/select.js
@@ -1,20 +1,43 @@
-import {html, render} from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js"
+import {html, render} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js"
 
 // testing with websockets -----------
 var socket = io()
 // testing with websockets end ------
 
+function click(e) {
+    e.preventDefault()
+
+    // save these and they will act as codes to get into servers
+    // though do be careful of having numbers less than total 
+    // number of digits (i.e. should be >1000 but is 809)
+    
+    // convert it into a string and append a 0 to the start
+    // depending on how different it is
+    console.log(e.target)
+    console.log(e.target.id)
+}
+
 function selectServer(s) {
     const template = () => html`
         <ul>
-            ${s.map((server) => html`<li>${server}</li>`)}
+            ${s.map((server) => html`
+
+            <li>
+            ${server}
+            <button id=${server} @click=${click}>Join Server</button>
+            </li>
+
+            `)}
         </ul>
     `
     const container = document.getElementById("select")
     render(template(), container)
 }
 
-socket.emit("server-grab")
+// after web page is fully loaded, emit socket to grab all servers
+htmx.on("htmx:afterSettle", function(e) {
+    socket.emit("server-grab")
+})
 
 socket.on("server-render", (servers) => {
     console.log(servers)
